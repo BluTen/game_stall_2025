e493ba3b0b30faf0eb7887b5a3fc8048dd2cb443
BluTen <nayanshetty2006@gmail.com>
2025-11-27 21:03:13 +0530

    Add leaderboard

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,7 +1,10 @@
 #define S1_BUTTON_PIN 5
 #define S2_BUTTON_PIN 6
 #define S3_BUTTON_PIN 7
-#define SC0_SH0 
+
+#define MIN_DUR  150  // The fastest the game is allowed to start
+#define INIT_DUR 850  // Duration added to minimum at the start of the game
+#define DUR_DCAY .97  // Rate of decay of duration
 
 void setup() {
     Serial.begin(9600);
@@ -18,8 +21,8 @@ void setup() {
     pinMode(11, OUTPUT);
     pinMode(12, OUTPUT);
     pinMode(13, OUTPUT);
-    PORTD = PORTD | B11100000;
-    PORTB = PORTB | B00111111;
+    PORTD = PORTD | 0b11100000;
+    PORTB = PORTB | 0b00111111;
 
     digitalWrite(8, HIGH);
     
@@ -28,24 +31,46 @@ void setup() {
 
 void loop() {
     // Busy wait until we recive start command from serial
-    while (!Serial.available());
+    byte _i = 0;
+    while (!Serial.available()) {
+        unsigned long last_millis = millis();
+        unsigned int bits;
+        bits = (1 << 8 - (_i % 3)) | (1 << 5 - ((_i+1) % 3)) | (1 << 2 - ((_i+2) % 3));
+        setScreen(bits);
+        while (millis() - last_millis < 1000) {
+            if (Serial.available()) goto game_start;
+        }
+        _i += 1;
+    }
+    
+game_start:
     // If the data from serial data is anything but the letter 'S' restart
     if (!(Serial.available() == 1 && Serial.read() == 'S')) goto end;
-
-    Serial.write('.');
     randomSeed(analogRead(0));
+    Serial.println("#Game started");
 
     // Init game state
     byte score = 0;
-    int duration = 3000;
+    float duration = MIN_DUR + INIT_DUR;
     byte screen = 0;
     byte shape = 0;
     long start_time = 0;
     bool timeout = true;
     // Some fun patterns before game starts
+    setScreen(0b100100100);
+    delay(1000);
+    setScreen(0b100100100 >> 1);
+    delay(1000);
+    setScreen(0b100100100 >> 2);
+    delay(1000);
+    setScreen(0);
+    delay(1000);
 
     // Gameplay loop
     while (true) {
+        duration = MIN_DUR + INIT_DUR * pow(DUR_DCAY, score);
+        Serial.write('D');
+        Serial.println(duration);
         screen = random(0,3);
         shape = random(0,3);
         timeout = true;
@@ -55,11 +80,11 @@ void loop() {
         while (millis() - start_time < duration) {
             if (PIND & B00011100) {
                 if (PIND & (B00000100 << shape)) {
+                    Serial.write('H');
                     score += 1;
+                    setScreen(0);
                     timeout = false;
-                    showShape(8,8);
-                    delay(500);
-                    break;
+                    delay(750);                 
                 }
                 break;
             }
@@ -67,21 +92,29 @@ void loop() {
         if (timeout) break;
     }
     
-
+    Serial.write('X');
+    Serial.println(score);
+    
+    
 end:
     Serial.println("#Ended, Restarting");
 }
 
 void showShape(byte screen, byte shape) {
-    Serial.print("#");
+    Serial.write('#');
     Serial.print(screen);
     Serial.print(".");
     Serial.println(shape);
     if (screen == 0) {
-        PORTD = (PORTD & B00011111) | (B00100000 << shape);
-        PORTB = PORTB & B11000000;
+        PORTD = (PORTD & 0b00011111) | (0b00100000 << shape);
+        PORTB = PORTB & 0b11000000;
     } else {
-        PORTB = (PORTB & B11000000) | (B00000001 << (3*(screen-1) + shape));
-        PORTD = PORTD & B00011111;
+        PORTB = (PORTB & 0b11000000) | (0b00000001 << (3*(screen-1) + shape));
+        PORTD = PORTD & 0b00011111;
     }
+}
+
+void setScreen(unsigned int bits) {
+    PORTB = (PORTB & 0b11000000) | (bits &  0b00111111);
+    PORTD = (PORTD & 0b00011111) | (bits & 0b111000000)>>1;
 }
--- server/main.py
+++ server/main.py
@@ -1,22 +1,156 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse, FileResponse
+import threading
+import time
+import json
+import requests
+import dotenv
 
-server = FastAPI()
+import serial
+from flask import Flask, current_app
+
+""" Disable Flask's default logging to keep the output clean """
+import logging  # noqa: E402
+
+log = logging.getLogger("werkzeug")
+log.setLevel(logging.ERROR)
 
 
-@server.get("/", response_class=HTMLResponse)
-def root():
-    with open("html/index.html", "r") as file:
-        resp = file.read()
-    return resp
+app = Flask(__name__)
+arduino = serial.Serial(port="COM5", baudrate=9600, timeout=None)
+SUPABASE_URL = "https://onficegxsclrxpaejfzx.supabase.co/rest/v1/"
+supabase_db = requests.Session()
+supabase_db.headers.update(
+    {
+        "apikey": dotenv.get_key(".env", "SUPABASE_SERVICE_ROLE_KEY"),
+    }
+)
+
+website_queue = []
 
 
-@server.get("/leaderboard")
-def get_leaderboard():
-    return FileResponse("leaderboard.json")
+@app.route("/")
+def home():
+    return current_app.send_static_file("index.html")
+
+
+@app.route("/events", methods=["GET"])
+def events():
+    def event_stream():
+        global website_queue
+        while True:
+            if website_queue:
+                event = website_queue.pop(0)
+                yield f"data: {event[0]}\n{json.dumps(event[1])}\n\n"
+            else:
+                time.sleep(0.1)
+
+    return app.response_class(event_stream(), mimetype="text/event-stream")
+
+
+def cmd_interface():
+    # Wait for server to start
+    global website_queue
+
+    time.sleep(2)
+    while True:
+        while not arduino.is_open:
+            input("Arduino not connected. Press ENTER to retry...")
+            try:
+                arduino.open()
+            except serial.SerialException:
+                continue
+
+        name = input("Enter name: ")
+        class_ = input("Enter class: ")
+
+        arduino.write(b"S")
+        website_queue.append(
+            (
+                "start-game",
+                {
+                    "name": name,
+                    "class": class_,
+                },
+            )
+        )
+        # TODO: Add game start music
+
+        while True:
+            duration = arduino.readline()
+            while duration[0] == ord("#"):
+                print("arduino:", duration[1:].strip().decode())
+                duration = arduino.readline()
+
+            if duration[0] != ord("D"):
+                print(
+                    f"Got '{duration.decode()}'. Desync between arduino and computer. Resetting, reset arduino manually..."
+                )
+                break
+            try:
+                duration = float(duration[1:].strip())
+            except ValueError:
+                print("Unable to get duration. Resetting, reset arduino manually...")
+                break
+            # TODO: add gameplay music
+            next_event = arduino.read()[0]
+            while next_event == ord("#"):
+                print("arduino:", arduino.readline().strip().decode())
+                next_event = arduino.read()[0]
+            if next_event == ord("H"):
+                # TODO: add hit sound sfx
+                print("Hit!")
+                website_queue.append(
+                    (
+                        "increment-score",
+                        {},
+                    )
+                )
+                continue
+            elif next_event == ord("R"):
+                print("Missed but life left restarting...")
+                website_queue.append(
+                    (
+                        "reset-score",
+                        {},
+                    )
+                )
+                continue
+            elif next_event == ord("X"):
+                # TODO: Game over sfx
+                print("Game over!")
+                score = arduino.readline().strip()
+                try:
+                    score = int(score)
+                except ValueError:
+                    print(f"Got: '{score.decode()}'")
+                    print("Unable to get score. Resetting, reset arduino manually...")
+                    break
+                print(f"Score: {score}")
+                print("Checking database for name...")
+                # Store data to be sent to website
+                supabase_db.post(
+                    SUPABASE_URL + "leaderboard",
+                    json={
+                        "name": name,
+                        "class": class_,
+                        "score": score,
+                    },
+                )
+                # Send ping to local page to update its display
+                website_queue.append(
+                    (
+                        "end-score",
+                        {
+                            "name": name,
+                            "class": class_,
+                            "score": score,
+                        },
+                    )
+                )
+                break
+
+        time.sleep(1)
 
 
 if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(server)
+    threading.Thread(target=cmd_interface, daemon=True).start()
+    app.run()
